49cb8613052128b5f66ea684e8128322cb131deb
JoaoVSerrano <jvserrano735@gmail.com>
2025-08-26 00:41:42 +0000

    Aula 03 - 25/08/2025

---
+++ src/main/java/app/Filme.java
@@ -0,0 +1,8 @@
+package app;
+
+public class Filme {
+    private long id;
+    private String titulo;
+    private Genero genero;
+    
+}
--- src/main/java/app/Hello.java
+++ src/main/java/app/Hello.java
@@ -2,7 +2,14 @@ package app;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo!!");
+        Genero g1 = new Genero(1, "Aventura");
+        Genero g2 = new Genero(2, " ");
+  
+        System.out.println("genero 1 = " + g1.getNome() + "(" + g1.getId() + ")");
+        System.out.println("genero 2 = " + g2.getNome() + "(" + g2.getId() + ")");
     }
 }
+
+
+
 
